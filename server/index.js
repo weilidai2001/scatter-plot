d6dc0e6d740b708bbba554b18da2b6859ffcf3ee
Weili Dai <weili.dai@turner.com>
2017-03-31 16:29:58 +0100

    Fixed CSV loading

--- server/index.js
+++ server/index.js
@@ -6,26 +6,25 @@ const app = express();
 
 let data = [];
 
-app.use('/load', (req, res) => {
-    csvtojson()
-        .fromFile('./data.csv')
-        .on('json', ({x, y}) => {
-            console.log('called')
-            data.push({
-                type: 'One',
-                x,
-                y
-            })
+csvtojson()
+    .fromFile('./data.csv')
+    .on('json', ({x, y}) => {
+        data.push({
+            type: 'One',
+            x,
+            y
         })
-        .on('done', () => {
+    })
+    .on('done', (error) => {
+        if (error) {
+            console.log('Unable to load CSV file')
+        } else {
+            app.use('/scatter-data', (req, res) => res.json(data));
 
-        })
+            app.use('/', (req, res) => res.json({
+                status: 'OK'
+            }));
 
-    res.json(data);
-});
-
-app.use('/', (req, res) => res.json({
-    status: 'OK'
-}));
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+            app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+        }
+    });
