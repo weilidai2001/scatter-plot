7c5a03e30fa704b42ffdceaf33afdef19fb3a724
Weili Dai <weili.dai@turner.com>
2017-03-31 10:11:07 +0100

    Added CSV import

--- server/index.js
+++ server/index.js
@@ -1,8 +1,28 @@
 import express from 'express';
+import csvtojson from 'csvtojson';
 
 const PORT = 3001;
 const app = express();
 
+let data = [];
+
+app.use('/load', (req, res) => {
+    csvtojson()
+        .fromFile('./data.csv')
+        .on('json', ({x, y}) => {
+            console.log('called')
+            data.push({
+                type: 'One',
+                x,
+                y
+            })
+        })
+        .on('done', () => {
+
+        })
+
+    res.json(data);
+});
 
 app.use('/', (req, res) => res.json({
     status: 'OK'
