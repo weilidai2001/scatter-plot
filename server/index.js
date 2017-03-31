8779fd3c869d8780b1633ae7f2f0c2da89c235bd
Weili Dai <weili.dai@turner.com>
2017-03-31 16:31:17 +0100

    Fix CSV formatting

--- server/index.js
+++ server/index.js
@@ -25,6 +25,6 @@ csvtojson()
                 status: 'OK'
             }));
 
-            app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+            app.listen(PORT, () => console.log(`Data loaded. Server is running on port ${PORT}`));
         }
     });
