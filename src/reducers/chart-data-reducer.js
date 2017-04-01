011d8d28e4d103b50f27ce8d4de49da4b325d687
Weili Dai <weili.dai@turner.com>
2017-04-01 13:43:46 +0100

    Remove limit

--- src/reducers/chart-data-reducer.js
+++ src/reducers/chart-data-reducer.js
@@ -3,7 +3,7 @@ import * as types from '../actions/action-types';
 export default function chartDataReducer(state = [], action) {
     switch (action.type) {
         case types.LOAD_CHART_DATA_SUCCESS:
-            return action.data.slice(0, 5000);
+            return action.data;
 
         default:
             return state;
